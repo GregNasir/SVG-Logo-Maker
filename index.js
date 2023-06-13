8e8a49439470fd8924def1dafcf06d5f3fa1ac49
GregNasir <63434657+GregNasir@users.noreply.github.com>
2023-06-13 13:01:10 -0500

    defining shapes

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const fs = require("fs");
 // Importing polygon classes
 const { Triangle, Square, Circle } = require("./lib/shapes");
 
-// Function writes the SVG file using user answers from inquirer prompts
+// wrtites SVG file
 function writeToFile(fileName, answers) {
-  // File starts as an empty string
   let svgString = "";
-  // Sets width and height of logo container
+  // Sets width and height
   svgString =
     '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
   // <g> tag wraps <text> tag so that user font input layers on top of polygon -> not behind
   svgString += "<g>";
-  // Takes user input for shape choice and inserts it into SVG file
+  // User input defines shape
   svgString += `${answers.shape}`;
 
   // Conditional check takes users input from choices array and then adds polygon properties and shape color to SVG string
@@ -32,7 +31,7 @@ function writeToFile(fileName, answers) {
     svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
   }
 
-  // <text> tag gives rise to text alignment, text-content/text-color taken in from user prompt and gives default font size of "40"
+  // takes text and makes default size 40px
   svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
   // Closing </g> tag
   svgString += "</g>";
